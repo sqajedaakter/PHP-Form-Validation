b416988672f8fcb0cafa7beabd6cdb09539eb062
sqajedaakter <sqajedaakter@gmail.com>
2022-06-07 12:56:30 +0600

    php error

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+$name_error = $phone_error = $email_error = "";
 $name = $phone = $email = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(empty($_POST['name'])){
+        $name_error = "Name field is required";
+    }else{
+        $name = test($_POST['name']);
+    }
+    if(empty($_POST['phone'])){
+        $name_error = "phone field is required";
+    }else{
+        $name = test($_POST['phone']);
+    }
+    if(empty($_POST['email'])){
+        $name_error = "email field is required";
+    }else{
+        $name = test($_POST['email']);
+    }
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $name = test($_POST['name']);
@@ -28,8 +47,11 @@ return $data;
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
 Name : <input type="text" name="name"/><br/><br/>
+<span style="color: red;"><?php echo $name_error;?></span><br/>
 Phone : <input type="text" name="phone"/><br/><br/>
+<span style="color: red;"><?php echo $phone_error;?></span><br/>
 Email : <input type="email" name="email"/><br/><br/>
+<span style="color: red;"><?php echo $email_error;?></span><br/>
 <input type="submit" name="btn" value="Submit"/>
 </form>
 <?php
